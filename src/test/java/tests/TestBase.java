5585216153f69c2a52f3c4bba2e52ca450ae1dcd
Станислав <stanislavkovalev.m@gmail.com>
2020-08-31 16:15:06 +0300

    Add helpers

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,12 +1,15 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static helpers.AttachmentHelper.*;
+import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class TestBase {
 
@@ -20,6 +23,12 @@ public class TestBase {
 
     @AfterEach
     public void finish() {
+        attachScreenshot("Last screenshots");
+        attachPageSource();
+        attachAsText("Browser console logs",getConsoleLogs());
         closeWebDriver();
+    }
+    public static String getConsoleLogs(){
+        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
     }
 }
